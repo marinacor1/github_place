4ba50850bcb5072b7346356c7615bf1c820c3c50
marinacor1 <marinaspam1234@gmail.com>
2016-06-05 17:39:26 -0600

    User image name and screen name appear once logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do                         # ~> NameError: uninitialized constant Rails
   root "users#show"
   get "/auth/github", as: :github_login
   get "/auth/github/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
 end
+
+# ~> NameError
+# ~> uninitialized constant Rails
+# ~>
+# ~> /Users/marinacorona/Turing/3Module/github_place/config/routes.rb:1:in `<main>'
